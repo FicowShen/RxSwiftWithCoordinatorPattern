43178cc59fe7443889807a59df846e518520bc4f
fshen <fshen@xogrp.com>
2019-03-14 18:48:43 +0800

    Initial Commit

---
+++ RxSwiftWithCoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftWithCoordinatorPattern
+//
+//  Created by fshen on 2019/3/14.
+//  Copyright © 2019 fshen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
